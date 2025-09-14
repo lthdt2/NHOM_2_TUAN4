81ef51784fa08cb308bd0e15437e76c9a36cd42a
tranhanh27012006-blip <tranhanh27012006@gmail.com>
2025-09-14 22:38:25 +0700

    Add files via upload

---
+++ baitaptuan3th.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author Lenovo
+ */
+public class baitaptuan3th {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String ten = "Hanh";
+        int tuoi = 20;
+        double ChieuCao =1.60;
+        boolean bietlaptrinh = true ;
+        System.out.println("Tên;"+ten);
+        System.out.println("Tuổi:"+tuoi);
+        System.out.println("chiều cao:"+ChieuCao);
+        System.out.println("Biết Lập Trình"+bietlaptrinh);
+    }
+    
+}
